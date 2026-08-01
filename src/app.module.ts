7fe6bf920bf9980edd3a016663168eb9a271c7b9
unknown <youremail@example.com>
2026-08-01 20:33:02 +0600

    connecting the postgres database

---
+++ src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PaymentsModule } from './payments/payments.module';
+import { AdminModule } from './admin/admin.module';
+import { VendorModule } from './vendor/vendor.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+    AuthModule,
+    UsersModule,
+    TicketsModule,
+    BookingsModule,
+    PaymentsModule,
+    AdminModule,
+    VendorModule,
+  ],
+})
+export class AppModule {}
